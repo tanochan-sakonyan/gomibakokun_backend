4520f7d167d1a483b62362a9ee00861bc86710bd
unagami <kantaunagami@gmail.com>
2025-06-08 23:23:07 +0900

    :construction: - domainのtrashcan構造体化からjsonとfirestoreを分離 - trashcan_config.goを追加 - newTrashcan関数を追加 - trashcanのvalidationを追加

---
+++ domain/error.go
@@ -0,0 +1,7 @@
+package domain
+
+import "errors"
+
+var (
+	ErrInvalidInput = errors.New("invalid input")
+)
--- domain/trashcan.go
+++ domain/trashcan.go
@@ -1,12 +1,29 @@
 package domain
 
 type Trashcan struct {
-	ID              string   `firestore:"ID"              json:"ID"`
-	Latitude        float64  `firestore:"latitude"        json:"latitude"`
-	Longitude       float64  `firestore:"longitude"       json:"longitude"`
-	Image           string   `firestore:"image"           json:"image"`
-	TrashType       []string `firestore:"trashType"       json:"trashType"`
-	NearestBuilding string   `firestore:"nearestBuilding" json:"nearestBuilding"`
-	Note            string   `firestore:"note"            json:"note"`
-	SelectedButton  string   `firestore:"selectedButton"  json:"selectedButton"`
+	id              string
+	latitude        float64
+	longitude       float64
+	image           string
+	trashType       []string
+	nearestBuilding string
+	note            string
+	selectedButton  string
+}
+
+func NewTrashcan(config *TrashcanConfig) (*Trashcan, error) {
+	if err := ValidateTrashcanConfig(config); err != nil {
+		return nil, err
+	}
+
+	return &Trashcan{
+		id:              config.ID,
+		latitude:        config.Latitude,
+		longitude:       config.Longitude,
+		image:           config.Image,
+		trashType:       config.TrashType,
+		nearestBuilding: config.NearestBuilding,
+		note:            config.Note,
+		selectedButton:  config.SelectedButton,
+	}, nil
 }
